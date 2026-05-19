e3c3e52ca7db7e3d5d40402112d0137fb8b9e4a7
Debbiejerry <hisdelightdeborah@gmail.com>
2026-05-19 13:17:25 +0100

    header component

---
+++ src/Header.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import reactLogo from './assets/react.svg'
+
+function Header() {
+  return (
+    <>
+        <header className="App-header">
+            <img src={reactLogo} alt="React Logo" />
+            <h2>React Fun-facts</h2>
+
+            <ul className="Menu">
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+      </header>
+    </>
+    
+  )
+}
+
+export default Header
